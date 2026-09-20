961fc75bcb2bb9c82b6eb99f9a1522779b9ef960
ashikeelahi3 <ashikeelahi3@gmail.com>
2026-09-20 22:03:21 +0600

    More fix in problem 02, 409

--- 409/Problem_02.R
+++ 409/Problem_02.R
@@ -6,28 +6,28 @@ i <- 0.07
 n <- 10
 m <- 12
 
-# im <- (1 + i)^(1/m) - 1
-# R <- P / annuity(i=im, n = n*m, k=1, type = "immediate")
-R <- (1/m) * P / annuity(i = i, n = n, k = m, type = "immediate")
+im <- (1+i)^(1/m) - 1
+
+R = (1/m) * P / annuity(i = i, n = n, k = m, type = "immediate")
 R 
 
 balance <- P
 loan <- data.frame()
 
-for (m0 in 1:(n*m)) {
+for (mo in 1:(n*m)) {
   interest <- balance * im
   capital <- R - interest
   balance <- balance - capital
-  loan <- rbind(loan, c(m0, R, interest, capital, balance))
+  loan <- rbind(loan, c(mo, R, interest, capital, balance))
 }
 
 names(loan) <- c("Month","Payment","Interest","Capital","Balance")
-loan
+round(loan, 2)
 loan[30, ]
 
 
 # (b) Saving scheme
-person <- LETTERS[1:5]
+person <- LETTERS[1:5] # c("A", "B", "C", "D", "E")
 P <- c(150000,120000,200000,100000,160000)
 i <- c(.07,.06,.08,.075,.08)
 n <- c(10,12,15,10,20)
@@ -48,8 +48,8 @@ data.frame(
 
 
 # (d) Effective annual rate
-(1 + .21/m)^m - 1
+(1 + 0.21/m)^m - 1
 
 
 # (e) Future value
-1000 * accumulatedValue(.03, 5) * 1.03
+1000 * accumulatedValue(0.03, 5) * 1.03
