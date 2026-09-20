9f6a5cc908f75861d09fab008a38e5243446c8f9
ashikeelahi3 <ashikeelahi3@gmail.com>
2026-09-20 19:56:35 +0600

    add problem 01

--- 409/Problem_01.R
+++ 409/Problem_01.R
@@ -14,7 +14,7 @@ print(exampleAct)
 
 # c) Calculate the probability that a person age 5 will survive 1 year.
 DemoEX1<-pxt(lifetable, 5,1)
-c(DemoEX1)
+DemoEX1
 
 # d) Calculate the probability that a person age 5 die within 1 year
 DemoEX2<-qxt(lifetable, 5, 1)
@@ -26,8 +26,8 @@ ans=1-(l6-l7)/l5
 ans
 
 
-# f) Calculate the probability that a person age 5 will not die between age 6 and 7.
-DemoEX3<-exn(lifetable, 5, 1, "complete")
+# f) Calculate the complete expectation of life at age 5.
+DemoEX3<-exn(lifetable, 5, type="complete")
 c(DemoEX3)
 
 # g) Calculate the central death rate at age 5.
@@ -47,9 +47,11 @@ c(DemoEX10)
 
 # j) From actuarial table, evaluate the value of a 15 years term life insurance for an aged 25
 Axn(actuarialtable=exampleAct, x=25, n=15)
+Axn(actuarialtable=exampleAct, x=10, n=15)
 
 # k) Evaluate the value of a 10 year term life insurance for an aged 22 with interest rate 6%.
-Axn(actuarialtable=exampleAct, x=22, n=12,i=0.06)
+Axn(actuarialtable=exampleAct, x=22, n=10, i=0.06)
+Axn(actuarialtable=exampleAct, x=10, n=12, i=0.06)
 
 # l) Calculate the value of the following assurances using actuarial table from (b) on a life aged 10, the sum assured being payable at the end of the year of death
 # i) Whole-life assurance of 1000
@@ -61,11 +63,9 @@ PE10_6 <- 500 * Exn(actuarialtable = exampleAct, x = 10, n = 6)
 PE10_6
 
 # iii) Temporary (term) assurance of 1000, term 6 years
-Term10_6 <- 1000 * Axn(actuarialtable = exampleAct, x = 10, n = 6, type = "term")
+Term10_6 <- 1000 * Axn(actuarialtable = exampleAct, x = 10, n = 6)
 Term10_6
 
-# iv) Endowment assurance of 100, term 5 years
-End10_5 <- 100 * Axn(actuarialtable = exampleAct, x = 10, n = 5, type = "EA")
+# iv) Endowment assurance of 1000, term 5 years
+End10_5  <- 1000  * AExn(actuarialtable = exampleAct, x = 10, n = 5)
 End10_5
-
-
